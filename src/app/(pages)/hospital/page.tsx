ce1fbd5ec0ef4916b7e24b585acbf2817e9e1276
Sagar Mainali <sagarmainali78@gmail.com>
2025-05-27 16:08:01 +0545

    added new filter category - length of stay in page/hospital

--- src/app/(pages)/hospital/page.tsx
+++ src/app/(pages)/hospital/page.tsx
@@ -34,7 +34,10 @@ export default function Hospital() {
     const getPatientByCategory = (category: string) => {
         if (!patientsData_All) return; // stop function execution if there is no data
 
-        const patientCount_byCategory: Record<string, number> = {};
+        // for objects where order doesn't matter
+        const patientCount_byOtherCategory: Record<string, number> = {};
+
+        // separate objects to preserve order of the properties
         const patientCount_byAge = {
             '0-15': 0,
             '15-30': 0,
@@ -42,37 +45,59 @@ export default function Hospital() {
             '45-60': 0,
             '60-75': 0,
             '75+': 0,
-        } // separate object to preserver order of the properties
+        }
+
+        const patientCount_byLengthOfStay = {
+            '0-3': 0,
+            '3-7': 0,
+            '7-14': 0,
+            '14-30': 0,
+            '30-45': 0,
+            '45-60': 0,
+            '60-75': 0,
+            '75+': 0,
+        }
 
         patientsData_All?.forEach(patientRecord => {
-            const selectedCategory = patientRecord[category as ('Condition' | 'Age' | 'Procedure')]; //get selected category from the row
+            const selectedCategory = patientRecord[category as ('Condition' | 'Procedure' | 'Age' | 'Length_of_Stay')]; //get selected category from the row
 
-            if (category !== 'Age') {
-                patientCount_byCategory[selectedCategory] = (patientCount_byCategory[selectedCategory] || 0) + 1;
-            } else {
+            if (category === 'Condition' || category === 'Procedure') {
+                patientCount_byOtherCategory[selectedCategory] = (patientCount_byOtherCategory[selectedCategory] || 0) + 1;
+            }
+            else if (category === 'Age') {
                 const age = patientRecord['Age'];
-                if (age < 15) {
-                    patientCount_byAge['0-15']++;
-                } else if (age < 30) {
-                    patientCount_byAge['15-30']++;
-                } else if (age < 45) {
-                    patientCount_byAge['30-45']++;
-                } else if (age < 60) {
-                    patientCount_byAge['45-60']++;
-                } else if (age < 75) {
-                    patientCount_byAge['60-75']++;
-                } else {
-                    patientCount_byAge['75+']++;
-                }
+
+                if (age < 15) patientCount_byAge['0-15']++;
+                else if (age < 30) patientCount_byAge['15-30']++;
+                else if (age < 45) patientCount_byAge['30-45']++;
+                else if (age < 60) patientCount_byAge['45-60']++;
+                else if (age < 75) patientCount_byAge['60-75']++;
+                else patientCount_byAge['75+']++;
+            }
+            else if (category === 'Length of Stay') {
+                const lengthOfStay = patientRecord['Length_of_Stay'];
+
+                if (lengthOfStay < 3) patientCount_byLengthOfStay['0-3']++;
+                else if (lengthOfStay < 7) patientCount_byLengthOfStay['3-7']++;
+                else if (lengthOfStay < 14) patientCount_byLengthOfStay['7-14']++;
+                else if (lengthOfStay < 30) patientCount_byLengthOfStay['14-30']++;
+                else if (lengthOfStay < 45) patientCount_byLengthOfStay['30-45']++;
+                else if (lengthOfStay < 60) patientCount_byLengthOfStay['45-60']++;
+                else if (lengthOfStay < 75) patientCount_byLengthOfStay['60-75']++;
+                else patientCount_byLengthOfStay['75+']++;
             }
         })
 
-        const patientCount_byCategory_formatted = Object.entries(category === 'Age' ? patientCount_byAge : patientCount_byCategory).map(
-            ([category, count]) => ({
-                category,
-                count
-            })
-        )
+        const patientCount_byCategory_formatted = Object.entries(category === 'Age'
+            ? patientCount_byAge
+            : category === 'Length of Stay'
+                ? patientCount_byLengthOfStay
+                : patientCount_byOtherCategory).map(
+                    ([category, count]) => ({
+                        category,
+                        count
+                    })
+                )
 
         setPatients_byCategory(patientCount_byCategory_formatted);
     }
@@ -88,7 +113,7 @@ export default function Hospital() {
             ? (
                 <div className='h-full w-full flex flex-col items-center gap-4'>
                     <div className='w-[90%] flex flex-row-reverse gap-2'>
-                        <CustomDropDown label='Category' arrowIcon={true} items={['Condition', 'Age', 'Procedure']} onClickHandler={setSelectedCategory} selectedValue={selectedCategory} />
+                        <CustomDropDown label='Category' arrowIcon={true} items={['Condition', 'Age', 'Procedure', 'Length of Stay']} onClickHandler={setSelectedCategory} selectedValue={selectedCategory} />
                     </div>
                     <ResponsiveContainer width="90%" height="80%">
                         <BarChart data={patients_byCategory} margin={{ left: 10, right: 10 }}>
@@ -103,13 +128,13 @@ export default function Hospital() {
                                 }
                             />
                             <YAxis />
-                            <Tooltip content={<CustomTooltip_Hospital selectedValue={selectedCategory}/>} />
+                            <Tooltip content={<CustomTooltip_Hospital selectedValue={selectedCategory} />} />
                             <Legend />
                             <CartesianGrid stroke="#f5f5f5" />
                             <Bar dataKey="count" fill="#4E6688" name="Number of patients" />
                         </BarChart>
                     </ResponsiveContainer>
-                    <p className='text-primary-gray text-[18px]'>Patients records categorized by <strong>'{(selectedCategory[0].toUpperCase() + selectedCategory.slice(1))}'</strong></p>
+                    <p className='text-primary-gray text-[18px]'>Number of patients categorized by <strong>'{(selectedCategory[0].toUpperCase() + selectedCategory.slice(1))}'</strong></p>
                 </div>
             )
             : <Loader />
