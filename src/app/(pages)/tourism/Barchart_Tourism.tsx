9ff6dc34b924c076936b360dda12049b3f1c756d
Sagar Mainali <sagarmainali78@gmail.com>
2026-09-17 20:16:36 +0545

    optimize: tourism page optimization

--- src/app/(pages)/tourism/Barchart_Tourism.tsx
+++ src/app/(pages)/tourism/Barchart_Tourism.tsx
@@ -1,12 +1,14 @@
-import Loader from '@/components/Loader';
+'use client'
+
 import { useState } from 'react'
 import { Bar, Brush, CartesianGrid, ComposedChart, Legend, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import CustomTooltip_Tourism from '@/app/(pages)/tourism/customTooltip_Tourism';
-import { getTourismData } from '@/lib/getTourismData';
 
-export default function Barchart_Tourism() {
-    const tourismData = getTourismData();
+type Barchart_TourismProps = {
+    tourismData: TourismDataT[]
+}
 
+export default function Barchart_Tourism({ tourismData }: Barchart_TourismProps) {
     const [selections, setSelections] = useState({
         type: 'stacked',
         showTrend: false,
@@ -36,43 +38,39 @@ export default function Barchart_Tourism() {
     }
 
     return (
-        tourismData
-            ? (
-                <div className='h-[90vh] w-full flex flex-col items-center gap-6 pt-6'>
-                    <div className='w-[90%] flex flex-col items-end gap-2 mr-[80px]'>
-                        <div className='view-options'> Type
-                            <span onClick={() => handleSelectionsChange('stacked')} className={selections.type === 'stacked' ? 'selected' : ''}>Stacked</span>
-                            <span onClick={() => handleSelectionsChange('separate')} className={selections.type === 'separate' ? 'selected' : ''}>Separate</span>
-                        </div>
-                        <div className='view-options'> View
-                            <span onClick={() => handleSelectionsChange('trend')} className={selections.showTrend ? 'selected' : ''}>Show trend</span>
-                        </div>
-                    </div>
-                    <ResponsiveContainer width="90%" height="70%">
-                        <ComposedChart data={tourismData} margin={{ left: 10, right: 40 }}>
-                            <XAxis dataKey="year" />
-                            <YAxis />
-                            <Tooltip content={<CustomTooltip_Tourism />} />
-                            <Legend />
-                            <CartesianGrid stroke="#f5f5f5" />
-                            <Bar dataKey="byAir.number" fill="#4E6688" name="Arrival by air" stackId={selections.type === 'stacked' ? 'a' : undefined} />
-                            <Bar dataKey="byLand.number" fill="#FE5D26" name="Arrival by Land" stackId={selections.type === 'stacked' ? 'a' : undefined} />
-                            {
-                                selections.showTrend && <Line dataKey="total" type="monotone" stroke="#328E6E" strokeWidth={2} name="Total arrivals" />
-                            }
-                            <Brush dataKey="year" height={30} stroke="#8884d8" />
-                        </ComposedChart>
-                    </ResponsiveContainer>
-                    <div className='w-[85%] relative flex justify-center items-center'>
-                        <p className='text-primary-gray text-[18px]'>Tourism Arrival in Nepal from <strong>1963</strong> to <strong>2023</strong></p>
-                        <div className='absolute right-0 top-[50%] -translate-y-[50%] flex flex-col text-[12px] text-gray-600'>
-                            <span>
-                                Source: <a href="https://www.tourism.gov.np/content/82/nepal-tourism-statistics-2023/" target='_blank' rel="noreferrer noopener" className='underline underline-offset-2'>Ministry of Culture, Tourism and Civil Aviation</a>
-                            </span>
-                        </div>
-                    </div>
+        <div className='h-[90vh] w-full flex flex-col items-center gap-6 pt-6'>
+            <div className='w-[90%] flex flex-col items-end gap-2 mr-[80px]'>
+                <div className='view-options'> Type
+                    <span onClick={() => handleSelectionsChange('stacked')} className={selections.type === 'stacked' ? 'selected' : ''}>Stacked</span>
+                    <span onClick={() => handleSelectionsChange('separate')} className={selections.type === 'separate' ? 'selected' : ''}>Separate</span>
                 </div>
-            )
-            : <Loader />
+                <div className='view-options'> View
+                    <span onClick={() => handleSelectionsChange('trend')} className={selections.showTrend ? 'selected' : ''}>Show trend</span>
+                </div>
+            </div>
+            <ResponsiveContainer width="90%" height="70%">
+                <ComposedChart data={tourismData} margin={{ left: 10, right: 40 }}>
+                    <XAxis dataKey="year" />
+                    <YAxis />
+                    <Tooltip content={<CustomTooltip_Tourism />} />
+                    <Legend />
+                    <CartesianGrid stroke="#f5f5f5" />
+                    <Bar dataKey="byAir.number" fill="#4E6688" name="Arrival by air" stackId={selections.type === 'stacked' ? 'a' : undefined} />
+                    <Bar dataKey="byLand.number" fill="#FE5D26" name="Arrival by Land" stackId={selections.type === 'stacked' ? 'a' : undefined} />
+                    {
+                        selections.showTrend && <Line dataKey="total" type="monotone" stroke="#328E6E" strokeWidth={2} name="Total arrivals" />
+                    }
+                    <Brush dataKey="year" height={30} stroke="#8884d8" />
+                </ComposedChart>
+            </ResponsiveContainer>
+            <div className='w-[85%] relative flex justify-center items-center'>
+                <p className='text-primary-gray text-[18px]'>Tourism Arrival in Nepal from <strong>1963</strong> to <strong>2023</strong></p>
+                <div className='absolute right-0 top-[50%] -translate-y-[50%] flex flex-col text-[12px] text-gray-600'>
+                    <span>
+                        Source: <a href="https://www.tourism.gov.np/content/82/nepal-tourism-statistics-2023/" target='_blank' rel="noreferrer noopener" className='underline underline-offset-2'>Ministry of Culture, Tourism and Civil Aviation</a>
+                    </span>
+                </div>
+            </div>
+        </div>
     )
 }
